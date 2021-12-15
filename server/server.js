e3757a9133421e261dea67379e54bdbb3f08b8aa
Aidan Amato <aidanamato@comcast.net>
2021-12-15 15:41:29 -0500

    initialize server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
